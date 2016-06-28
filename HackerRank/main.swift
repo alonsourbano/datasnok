5e437ecb32d286cbcf7c7744daf86ef1c0c314a0
Vladimir Urbano <l@pantanourbano.com>
2016-06-28 00:34:00 -0400

    Initial Commit

---
+++ HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Vladimir Urbano on 6/28/16.
+//  Copyright © 2016 Vladimir Urbano. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
