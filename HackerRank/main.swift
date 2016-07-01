3111e3fe84f7ee652878b422a07d0f2b562fdfb9
Vladimir Urbano <l@pantanourbano.com>
2016-07-01 10:10:51 -0400

    · 30 days of code · Day 4: Class vs. Instance

--- HackerRank/main.swift
+++ HackerRank/main.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Vladimir Urbano. All rights reserved.
 //
 
+// Warm up
 //SolveMeFirst()
 //SimpleArraySum()
 //AVeryBigSum()
@@ -13,10 +14,15 @@
 //PlusMinus()
 //Staircase()
 //TimeConversion()
+
+// Implementation
+//DivisibleSumPairs()
 //AngryProfessor()
+//SherlockAndTheBeast()
+
+// 30 days of code
 //Day0HelloWorld()
 //Day1DataTypes()
 //Day2Operators()
 //Day3IntroToConditionalStatements()
-//DivisibleSumPairs()
-//AngryProfessor()
+Day4ClassVsInstance()
