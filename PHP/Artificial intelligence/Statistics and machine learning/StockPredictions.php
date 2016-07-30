4a6917b626a82a4300eb8e5e3d143257b0c25602
Vladimir Urbano <l@pantanourbano.com>
2016-07-30 00:30:42 -0400

    no message

---
+++ PHP/Artificial intelligence/Statistics and machine learning/StockPredictions.php
@@ -0,0 +1,12 @@
+<?php
+# https://www.hackerrank.com/challenges/stockprediction
+
+$fp = fopen("php://stdin", "r");
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+$temp = fgets($fp);
+$temp = explode(' ', $temp);
+$m = $temp[0];
+$k = $temp[1];
+$d = $temp[2];
+?>
