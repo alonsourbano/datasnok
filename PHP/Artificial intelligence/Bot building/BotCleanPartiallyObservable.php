22c155e273f236f76cb90aad053e7f9e3f642989
Vladimir Urbano <l@pantanourbano.com>
2016-07-31 00:34:53 -0400

    Artificial intelligence > Bot building > BotClean partially observable

--- PHP/Artificial intelligence/Bot building/BotCleanPartiallyObservable.php
+++ PHP/Artificial intelligence/Bot building/BotCleanPartiallyObservable.php
@@ -1,33 +1,44 @@
 <?php
-// https://www.hackerrank.com/challenges/botcleanv2
+// https://www.hackerrank.com/challenges/botclean
 
-function findNextDirty($posr, $posc, &$board)
-{
-	if (boardIsClean($board)) {
-		return null;
+class Node {
+	var $children;
+	var $distance;
+	var $level;
+	var $r;
+	var $c;
+	var $board;
+	var $root;
+
+	function __construct($r, $c, $board)
+	{
+		$this->r = $r;
+		$this->c = $c;
+		$this->board = $board;
+		$this->children = [];
+		$this->root = false;
+		$this->distance = 0;
+		$this->level = 0;
+
+		$this->board[intval($r)][intval($c)] = '-';
 	}
 
-	$n = count($board);
+	function getDirties()
+	{
+		$d = [];
+		for ($i = 0; $i < count($this->board); $i++) {
+			for ($j = 0; $j < strlen($this->board[$i]); $j++) {
+				if ($this->board[$i][$j] == 'd') {
+					$d[] = [
+						'r' => $i,
+						'c' => $j
+					];
+				}
+			}
+		}
 
-	if ($posc < $n - 1 && $posc >= 0 && $posr >= 0 && $posr < $n && $board[$posr][$posc + 1] == 'd') {
-		return [$posr, $posc + 1];
-	} else if ($posr < $n - 1 && $posr >= 0 && $posc < $n - 1 && $posc >= 0 && $board[$posr + 1][$posc + 1] == 'd') {
-		return [$posr + 1, $posc + 1];
-	} else if ($posr < $n - 1 && $posr >= 0 && $posc >= 0 && $posc < $n && $board[$posr + 1][$posc] == 'd') {
-		return [$posr + 1, $posc];
-	} else if ($posr < $n - 1 && $posr >= 0 && $posc > 0 && $posc < $n && $board[$posr + 1][$posc - 1] == 'd') {
-		return [$posr + 1, $posc - 1];
-	} else if ($posc > 0 && $posc < $n && $posr >= 0 && $posr < $n && $board[$posr][$posc - 1] == 'd') {
-		return [$posr, $posc - 1];
-	} else if ($posr > 0 && $posr < $n && $posc > 0 && $posc < $n && $board[$posr - 1][$posc - 1] == 'd') {
-		return [$posr - 1, $posc - 1];
-	} else if ($posr > 0 && $posr < $n && $posc >= 0 && $posc < $n && $board[$posr - 1][$posc] == 'd') {
-		return [$posr - 1, $posc];
-	} else if ($posr > 0 && $posr < $n && $posc < $n - 1 && $posc >= 0 && $board[$posr - 1][$posc + 1] == 'd') {
-		return [$posr - 1, $posc + 1];
+		return $d;
 	}
-
-	return null;
 }
 
 function canMove(&$to, &$nextTo, $posr, $posc, $board)
@@ -74,164 +85,142 @@ function canMove(&$to, &$nextTo, $posr, $posc, $board)
 				$nextTo = $nextTo;
 				return false;
 			}
+
+		default:
+			file_put_contents('php://stderr', 'SWITCH DEFAULT');
+			break;
 	}
 
 	return false;
 }
 
-function boardIsClean($board) {
-	for ($i = 0; $i < count($board); $i++) {
-		for ($j = 0; $j < strlen($board[$i]); $j++) {
-			if ($board[$i][$j] == 'd') {
-				return false;
-			}
-		}
+function saveBoard($bh, $board)
+{
+	$save = implode("\n", $board);
+	file_put_contents($bh, $save);
+}
+
+function printNext($r, $c, $posr, $posc)
+{
+	if ($r < $posr) {
+		echo "UP\n";
+		return;
 	}
 
-	return true;
+	if ($c > $posc) {
+		echo "RIGHT\n";
+		return;
+	}
+
+	if ($r > $posr) {
+		echo "DOWN\n";
+		return;
+	}
+
+	if ($c < $posc) {
+		echo "LEFT\n";
+		return;
+	}
+
+	file_put_contents('php://stderr', 'PRINT NEXT NONE');
 }
 
 function next_move(&$posr, &$posc, &$board)
 {
-	$posr = intval($posr);
-	$posc = intval($posc);
-	$n = count($board);
-	if ($board[$posr][$posc] == 'd') {
-		print("CLEAN\n");
-		$board[$posr][$posc] = '-';
-	} else {
-		$next = findNextDirty($posr, $posc, $board);
-
-		$r = $posr;
-		$c = $posc;
-		$offset = 1;
-		$direction = 0;
-		$wh = 'move.txt';
-
-		while (empty($next)) {
-			if ($offset > 1) {
-				if (file_exists($wh)) {
-					$f = file_get_contents($wh);
-					$f = explode('-', $f);
-				}
-				if (!empty($f[0]) && !empty($f[1])) {
-					$to = $f[0];
-					$nextTo = $f[1];
-				} else {
-					$to = 'RIGHT';
-					$nextTo = 'DOWN';
-				}
-				$next = canMove($to, $nextTo, $posr, $posc, $board);
-				file_put_contents($wh, "{$to}-{$nextTo}");
-				continue;
-			}
-
-			if ($c < $n - 1 && $c >= 0 && $r >= 0 && $r < $n) {
-				$next = findNextDirty($r, $c + 1, $board);
-				if (!empty($next)) {
-					break;
+	$bh = 'board.txt';
+	if (file_exists($bh)) {
+		$savedBoard = file_get_contents($bh);
+		$savedBoard = explode("\n", $savedBoard);
+		for ($i = 0; $i < count($board); $i++) {
+			for ($j = 0; $j < strlen($board[$i]); $j++) {
+				if ($board[intval($i)][intval($j)] == 'o') {
+					$board[intval($i)][intval($j)] = $savedBoard[intval($i)][intval($j)];
 				}
 			}
-
-			if ($r < $n - 1 && $r >= 0 && $c < $n - 1 && $c >= 0) {
-				$next = findNextDirty($r + 1, $c + 1, $board);
-				if (!empty($next)) {
-					break;
-				}	
-			}
-
-			if ($r < $n - 1 && $r >= 0 && $c >= 0 && $c < $n) {
-				$next = findNextDirty($r + 1, $c, $board);
-				if (!empty($next)) {
-					break;
-				}	
-			}
-
-			if ($r < $n - 1 && $r >= 0 && $c > 0 && $c < $n) {
-				$next = findNextDirty($r + 1, $c - 1, $board);
-				if (!empty($next)) {
-					break;
-				}	
-			}
-
-			if ($c > 0 && $c < $n && $r >= 0 && $r < $n) {
-				$next = findNextDirty($r, $c - 1, $board);
-				if (!empty($next)) {
-					break;
-				}
-			}
-			
-			if ($r > 0 && $r < $n && $c > 0 && $c < $n) {
-				$next = findNextDirty($r - 1, $c - 1, $board);
-				if (!empty($next)) {
-					break;
-				}
-			}
-			
-			if ($r > 0 && $r < $n && $c >= 0 && $c < $n) {
-				$next = findNextDirty($r - 1, $c, $board);
-				if (!empty($next)) {
-					break;
-				}
-			}
-			
-			if ($r > 0 && $r < $n && $c < $n - 1 && $c >= 0) {
-				$next = findNextDirty($r - 1, $c + 1, $board);
-				if (!empty($next)) {
-					break;
-				}
-			}
-
-			switch ($direction % 8) {
-				case 0 :
-					$r = $posr + $offset;
-					$c = $posc;
-					break;
-				case 1 :
-					$r = $posr;
-					$c = $posc + $offset;
-					break;
-				case 2 :
-					$r = $posr - $offset;
-					$c = $posc;
-					break;
-				case 3 :
-					$r = $posr;
-					$c = $posc - $offset;
-					break;
-				case 4 :
-					$r = $posr + $offset;
-					$c = $posc + $offset;
-					break;
-				case 5 :
-					$r = $posr + $offset;
-					$c = $posc - $offset;
-					break;
-				case 6 :
-					$r = $posr - $offset;
-					$c = $posc - $offset;
-					break;
-				case 7 :
-					$r = $posr - $offset;
-					$c = $posc + $offset;
-					$offset++;
-					break;
-			}
-
-			$direction++;
-		}
-
-		if ($next[1] > $posc) {
-			print("RIGHT\n");
-		} else if ($next[0] > $posr) {
-			print("DOWN\n");
-		} else if ($next[1] < $posc) {
-			print("LEFT\n");
-		} else if ($next[0] < $posr) {
-			print("UP\n");
 		}
 	}
-	return 0;
+
+	saveBoard($bh, $board);
+
+	if ($board[intval($posr)][intval($posc)] == 'd') {
+		echo "CLEAN\n";
+		return;
+	}
+
+	$root = new Node($posr, $posc, $board);
+	$root->root = 7;
+	$q = [$root];
+	while (!empty($q)) {
+		$current = array_shift($q);
+		# Avoid timeout
+		if ($current->level > 4) {
+			continue;
+		}
+		$dirties = $current->getDirties();
+		foreach ($dirties as $dirty) {
+			$child = new Node($dirty['r'], $dirty['c'], $current->board);
+			$child->distance = $current->distance + abs($dirty['r'] - $current->r) + abs($dirty['c'] - $current->c);
+			$child->level = $current->level + 1;
+			if ($current->root === false) {
+				$child->root = [
+					'r' => $current->r,
+					'c' => $current->c
+				];
+			} else if ($current->root != 7) {
+				$child->root = $current->root;
+			}
+			$current->children[] = $child;
+			$q[] = $child;
+		}
+	}
+
+	$q = [$root];
+	$minV = 50;
+	$minP = [];
+	while (!empty($q)) {
+		$current = array_shift($q);
+		if (count($current->children) > 0) {
+			foreach ($current->children as $child) {
+				$q[] = $child;
+			}
+		} else {
+			if ($current->distance < $minV) {
+				$minV = $current->distance;
+				if (empty($current->root)) {
+					$minP = [
+						'r' => $current->r,
+						'c' => $current->c
+					];
+				} else {
+					$minP = $current->root;
+				}
+			}
+		}
+	}
+
+	if (empty($minP['r']) && empty($minP['c'])) {
+		$wh = 'move.txt';
+		$next = null;
+		while (empty($next)) {
+			if (file_exists($wh)) {
+				$f = file_get_contents($wh);
+				$f = explode('-', $f);
+			}
+			if (!empty($f[0]) && !empty($f[1])) {
+				$to = $f[0];
+				$nextTo = $f[1];
+			} else {
+				$to = 'RIGHT';
+				$nextTo = 'DOWN';
+			}
+			$next = canMove($to, $nextTo, $posr, $posc, $board);
+			file_put_contents($wh, "{$to}-{$nextTo}");
+		}
+		printNext($next[0], $next[1], $posr, $posc);
+		return;
+	}
+
+	printNext($minP['r'], $minP['c'], $posr, $posc);
 }
 
 $fp = fopen("php://stdin", "r");
