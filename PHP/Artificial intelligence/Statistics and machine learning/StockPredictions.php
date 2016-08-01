1ad3870c83b7886dab271de1563bba3ed3f953cf
Vladimir Urbano <l@pantanourbano.com>
2016-08-01 09:03:46 -0400

    Artificial intelligence > Statistics and machine learning > Stock prediction

--- PHP/Artificial intelligence/Statistics and machine learning/StockPredictions.php
+++ PHP/Artificial intelligence/Statistics and machine learning/StockPredictions.php
@@ -6,7 +6,15 @@ $fp = fopen("php://stdin", "r");
 
 $temp = fgets($fp);
 $temp = explode(' ', $temp);
-$m = $temp[0];
-$k = $temp[1];
-$d = $temp[2];
+$m = $temp[0]; # Amount of money you could spend that day
+$k = $temp[1]; # Number of different stocks available for buying or selling
+$d = $temp[2]; # Number of remaining days for trading stocks
+
+for ($i = 0; $i < $k; $i++) {
+    $temp = fgets($fp);
+    $temp = explode(' ', $temp);
+    $name = $temp[0]; # The name of the stock (a string).
+    $owned = $temp[1]; # The number of shares you own of that stock.
+    $prices = $temp[2]; # 5 space separated numbers representing the stock's price for the last 5 days. These are ordered from oldest to newest, so the last number is the current stock price
+}
 ?>
