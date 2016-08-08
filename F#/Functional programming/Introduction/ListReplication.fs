54b48d2df67afb7700a0fecae59d0193e2c18c35
Vladimir Urbano <l@pantanourbano.com>
2016-08-08 09:44:39 -0400

    Functional programming > Introduction > List replication

---
+++ F#/Functional programming/Introduction/ListReplication.fs
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/fp-list-replication
+
+open System
+open Microsoft.FSharp.Collections
+
+let solve s, l =
+    printf "Hola"
+
+[<EntryPoint>]
+let main argv =
+    let s = Console.ReadLine() |> int
+    let l = Console.ReadLine() |> List<int>
+    0
