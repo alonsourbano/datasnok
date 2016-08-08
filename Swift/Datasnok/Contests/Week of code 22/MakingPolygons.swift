e94b420beb7a269ead4fcb9ed52b49f9333f58e5
Vladimir Urbano <l@pantanourbano.com>
2016-08-08 10:21:03 -0400

    Contests > Week of code 22 > Making polygons

---
+++ Swift/Datasnok/Contests/Week of code 22/MakingPolygons.swift
@@ -0,0 +1,16 @@
+//
+//  MakingPolygons.swift
+//  Datasnok
+//
+//  Created by Vladimir Urbano on 8/8/16.
+//  Copyright © 2016 Vladimir Urbano. All rights reserved.
+//
+//
+
+import Foundation
+
+class MakingPolygons {
+	init() {
+		
+	}
+}
