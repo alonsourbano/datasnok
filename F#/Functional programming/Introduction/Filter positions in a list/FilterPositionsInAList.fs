9aa3a2bb647b6e4ea40c74c8a0ff83a3599041ac
Vladimir Urbano <l@pantanourbano.com>
2016-08-08 22:58:10 -0400

    WIP: Functional programming > Introduction > Filter positions in a list

--- F#/Functional programming/Introduction/Filter positions in a list/FilterPositionsInAList.fs
+++ F#/Functional programming/Introduction/Filter positions in a list/FilterPositionsInAList.fs
@@ -1,2 +1,22 @@
 // https://www.hackerrank.com/challenges/fp-filter-positions-in-a-list
 
+open System
+
+let solve(l: int list) =
+    
+    for i in l.Length - 1 .. -1 .. 0 do
+        let item = l.Item i
+        if i % 2 = 1 then
+            i.ToString() |> printfn "%s"
+
+let rec readLines l: int list =
+    let input = Console.ReadLine()
+    if String.IsNullOrEmpty input then
+        l
+    else
+        (input |> int) :: l |> readLines
+
+[<EntryPoint>]
+let main argv =
+    [] |> readLines |> solve
+    0
